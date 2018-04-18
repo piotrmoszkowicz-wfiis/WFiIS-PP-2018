38ff6d2dfeeb0d4763574f5b5cb22c77b459b06e
Piotr Moszkowicz <nommo_bd1@tlen.pl>
2018-04-19 00:12:06 +0200

    Added age

--- Zajęcia 7/1.c
+++ Zajęcia 7/1.c
@@ -1,33 +1,40 @@
 #include <stdio.h>
 #include <string.h>
 
-struct klient {
+struct klient
+{
   char imie[20];
   char nazwisko[32];
   char postcode[7];
+  int wiek;
 };
 
-int compare(const void* a, const void* b){
-  struct klient clientA = *(struct klient*)a;
-  struct klient clientB = *(struct klient*)b;
+int compare(const void *a, const void *b)
+{
+  struct klient clientA = *(struct klient *)a;
+  struct klient clientB = *(struct klient *)b;
 
   int cmp = strcmp(clientA.nazwisko, clientB.nazwisko);
 
-  if (cmp == 0){
+  if (cmp == 0)
+  {
     return strcmp(clientA.imie, clientB.imie);
   }
   return cmp;
 }
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
   struct klient clients[5];
-  int i=0;
+  int i = 0;
 
-  while(i<5){
+  while (i < 5)
+  {
     struct klient client;
     scanf("%s", client.imie);
     scanf("%s", client.nazwisko);
     scanf("%s", client.postcode);
+    scanf("%d", client.wiek);
 
     clients[i] = client;
     i++;
@@ -35,9 +42,9 @@ int main(int argc, char const *argv[]) {
 
   qsort(clients, 5, sizeof(clients[0]), compare);
 
-
-  for(i=0;i<5;i++){
-    printf("im: %s nazw: %s kod: %s\n", clients[i].imie, clients[i].nazwisko, clients[i].postcode);
+  for (i = 0; i < 5; i++)
+  {
+    printf("im: %s nazw: %s kod: %s wiek: %d\n", clients[i].imie, clients[i].nazwisko, clients[i].postcode, clients[i].wiek);
   }
 
   return 0;
